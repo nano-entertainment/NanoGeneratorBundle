b44b7d48f702e3cf377eff52a676ca4e42d17c7d
Lasse Nielsen <lasse.nielsen@me.com>
2013-01-30 18:27:44 +0100

    create bundle class

---
+++ NanoGeneratorBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Nano\Bundle\GeneratorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ *  The Nano Generator Bundle
+ *
+ * @author Lasse Nielsen <lasse.nielsen@nano-entertainment.de>
+ */
+
+class NanoGeneratorBundle extends Bundle
+{
+} 
