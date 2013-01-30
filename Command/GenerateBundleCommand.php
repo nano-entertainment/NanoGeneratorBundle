53df834aea534e3a5146f3afc5c2b3d5a8730844
Lasse Nielsen <lasse.nielsen@me.com>
2013-01-30 18:41:00 +0100

    create Generate Bundle Command

---
+++ Command/GenerateBundleCommand.php
@@ -0,0 +1,34 @@
+<?php
+namespace Nano\Bundle\GeneratorBundle\Command;
+
+use Sensio\Bundle\GeneratorBundle\Command\GenerateBundleCommand as SensioGenerateBundleCommand;
+use Nano\Bundle\GeneratorBundle\Generator\BundleGenerator;
+
+/**
+ * Generates bundles
+ *
+ * @author Lasse Nielsen <lasse.nielsen@nano-entertainment.de>
+ */
+class GenerateBundleCommand extends SensioGenerateBundleCommand
+{
+    protected $generator;
+
+    protected function configure()
+    {
+        parent::configure();
+
+        $this->setName('nano:generate:bundle');
+        $this->setDescription('Generate Bundle');
+
+    }
+
+    protected function getGenerator()
+    {
+        if (null === $this->generator) {
+          $this->generator = new BundleGenerator($this->getContainer()->get('filesystem'), __DIR__.'/../Resources/skeleton/bundle');
+        }
+
+        return $this->generator;
+    }
+
+}
