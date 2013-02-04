cb0ec6ef7db3d6cbfb88229bcf29738eeb574c3a
Lasse Nielsen <lasse.nielsen@me.com>
2013-02-04 22:07:55 +0100

    test names

--- Tests/Command/GenerateBundleCommandTest.php
+++ Tests/Command/GenerateBundleCommandTest.php
@@ -12,9 +12,9 @@ class GenerateBundleCommandTest extends SensioGenerateBundleCommandTest
         $tmp = sys_get_temp_dir();
 
         return array(
-            array(array('--dir' => $tmp), "Foo/BarBundle\n", array('Foo\BarBundle', 'FooBarBundle', $tmp.'/', 'annotation', false)),
-            array(array('--dir' => $tmp), "Foo/BarBundle\nBarBundle\nfoo\nyml\nn", array('Foo\BarBundle', 'BarBundle', 'foo/', 'yml', false)),
-            array(array('--dir' => $tmp, '--format' => 'yml', '--bundle-name' => 'BarBundle', '--structure' => true), "Foo/BarBundle\n", array('Foo\BarBundle', 'BarBundle', $tmp.'/', 'yml', true)),
+            array(array('--dir' => $tmp), "Foo/BarABundle\n", array('Foo\BarBundle', 'FooBarBundle', $tmp.'/', 'annotation', false)),
+            array(array('--dir' => $tmp), "Foo/BarBBundle\nBarBBundle\nfoo\nyml\nn", array('Foo\BarBBundle', 'BarBBundle', 'foo/', 'yml', false)),
+            array(array('--dir' => $tmp, '--format' => 'yml', '--bundle-name' => 'BarCBundle', '--structure' => true), "Foo/BarCBundle\n", array('Foo\BarCBundle', 'BarCBundle', $tmp.'/', 'yml', true)),
         );
     }
 
@@ -23,8 +23,8 @@ class GenerateBundleCommandTest extends SensioGenerateBundleCommandTest
         $tmp = sys_get_temp_dir();
 
         return array(
-            array(array('--dir' => $tmp, '--namespace' => 'Foo/BarBundle'), array('Foo\BarBundle', 'FooBarBundle', $tmp.'/', 'annotation', false)),
-            array(array('--dir' => $tmp, '--namespace' => 'Foo/BarBundle', '--format' => 'yml', '--bundle-name' => 'BarBundle', '--structure' => true), array('Foo\BarBundle', 'BarBundle', $tmp.'/', 'yml', true)),
+            array(array('--dir' => $tmp, '--namespace' => 'Foo/BarDBundle'), array('Foo\BarDBundle', 'FooBarDBundle', $tmp.'/', 'annotation', false)),
+            array(array('--dir' => $tmp, '--namespace' => 'Foo/BarEBundle', '--format' => 'yml', '--bundle-name' => 'BarEBundle', '--structure' => true), array('Foo\BarEBundle', 'BarEBundle', $tmp.'/', 'yml', true)),
         );
     }
 
